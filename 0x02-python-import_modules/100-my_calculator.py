4e7682b602ec24d9c3b01654db2a1f52c0bc44b5
AdiyoFouad <odjfouad38@gmail.com>
2022-12-01 11:46:44 +0100

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ = "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else :
-        op = sys.argv[2];
+        op = sys.argv[2]
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if op == '+':
