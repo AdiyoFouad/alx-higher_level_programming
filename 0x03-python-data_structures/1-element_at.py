fade6a874e0429a8c594f9a64a62b0000d97c33c
AdiyoFouad <odjfouad38@gmail.com>
2022-12-05 17:57:53 +0100

    Print element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if my_list == []:
+        return None
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
